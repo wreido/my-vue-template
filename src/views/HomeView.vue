f2658df8b5351a136f2cef48bcd661c5bd3951cf
ferdinand <983901430@qq.com>
2024-04-11 17:49:14 +0800

    fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import TheWelcome from '../components/TheWelcome.vue'
+
+const a = ref(0)
+
+const test = () => {
+  a.value = a.value + 1
+}
 </script>
 
 <template>
   <main>
+    {{ a }}
+    <div @click="test">按钮</div>
     <TheWelcome />
   </main>
 </template>
