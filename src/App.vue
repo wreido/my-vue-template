9e874c6ceac921ae50cc8a9553d15e45d6a501f4
ferdinand <983901430@qq.com>
2024-04-19 09:56:14 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@ const test = () => {
 
 <template>
   <div @click="test">按钮</div>
+  {{ user.count }}
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
