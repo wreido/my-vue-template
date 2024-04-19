f4e3400dba02a6741b6e6c03c96cf6065f013da9
ferdinand <983901430@qq.com>
2024-04-19 11:03:50 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,94 +1,14 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import useStore from '@/stores'
-const { user } = useStore()
-
-const test = () => {
-  user.increment()
-  console.log(user.count)
-}
-</script>
-
 <template>
-  <div @click="test">按钮</div>
-  {{ user.count }}
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <el-config-provider :locale="locale" :size="size" :z-index="zIndex">
+    <RouterView />
+  </el-config-provider>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script setup lang="ts">
+import { ref } from 'vue'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+const locale = zhCn
+const size = ref('small')
+const zIndex = ref(3000)
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,9 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-import TheWelcome from '../components/TheWelcome.vue'
-
-const a = ref(0)
-
-const test = () => {
-  a.value = a.value + 1
-}
-</script>
-
 <template>
   <main>
-    {{ a }}
-    <div @click="test">按钮</div>
     <TheWelcome />
   </main>
 </template>
+
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
