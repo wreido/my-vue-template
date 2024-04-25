745fb7db397e712d3a6ebf35eb0a7a8cd31a56e2
ferdinand <983901430@qq.com>
2024-04-25 18:02:26 +0800

    fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -26,3 +26,23 @@ const router = createRouter({
 export default router
 
 export { asyncRoutes }
+
+///11111111111111111111
+const comps = import.meta.glob('../views/**/index.vue')
+const pages = import.meta.glob('../views/**/index.config.ts', { eager: true, import: 'default' })
+const routes1 = Object.entries(pages).map(([path, meta]) => {
+  const compPath = path.replace('page.js', 'index.vue')
+  path = path.replace('../views', '').replace('/index.config.ts', '') || '/'
+  const name = path.split('/').filter(Boolean).join('-') || 'index'
+
+  return {
+    path,
+    name,
+    meta,
+    component: comps[compPath]
+  }
+})
+
+console.log(routes1)
+console.log(comps)
+console.log(pages)
