0f92926a31896f2f996cd7536461f7c7b0a7acff
ferdinand <983901430@qq.com>
2024-04-25 15:29:45 +0800

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-import { usePermission } from '@/stores'
-import type { RouteRecordRaw } from 'vue-router'
-import Layout from '@/layout/index.vue'
 
 import App from './App.vue'
 import router from './router'
-// import './permission'
+import init from './permission'
 import './assets/main.css'
 
 const app = createApp(App)
@@ -15,17 +12,7 @@ const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 app.use(pinia)
 
-const perms = ['shopping', 'shopping:goods', 'shopping:goods:list']
-const permission = usePermission()
-const accessRoutes: Array<RouteRecordRaw> = await permission.generateRoutes(perms)
-
-const flag: Array<RouteRecordRaw> = accessRoutes.flatMap((item) =>
-  item.children ? item.children.map((r: any) => ({ ...r, component: Layout })) : []
-)
-flag.forEach((routerItem) => router.addRoute(routerItem))
-
-console.log(4234234234)
+await init()
 
 app.use(router)
-
 app.mount('#app')
--- src/permission.ts
+++ src/permission.ts
@@ -3,22 +3,35 @@ import Layout from '@/layout/index.vue'
 import { usePermission } from '@/stores'
 import type { RouteRecordRaw } from 'vue-router'
 
-const whiteList = ['/login', '/auth-redirect']
+// const whiteList = ['/login', '/auth-redirect']
 
-const perms = ['shopping', 'shopping:goods', 'shopping:goods:list']
+// const perms = ['shopping', 'shopping:goods', 'shopping:goods:list']
 
-router.beforeEach(async (to, from, next) => {
-  if (!to.redirectedFrom) {
-    if (perms.length > 0) {
-      const permission = usePermission()
-      const accessRoutes: Array<RouteRecordRaw> = await permission.generateRoutes(perms)
-      const flag: Array<RouteRecordRaw> = accessRoutes.flatMap((item) =>
-        item.children ? item.children.map((r: any) => ({ ...r, component: Layout })) : []
-      )
-      flag.forEach((routerItem) => router.addRoute(routerItem))
-    }
-    next(to.path)
-  } else {
-    next()
-  }
-})
+// router.beforeEach(async (to, from, next) => {
+//   if (!to.redirectedFrom) {
+//     if (perms.length > 0) {
+//       const permission = usePermission()
+//       const accessRoutes: Array<RouteRecordRaw> = await permission.generateRoutes(perms)
+//       const flag: Array<RouteRecordRaw> = accessRoutes.flatMap((item) =>
+//         item.children ? item.children.map((r: any) => ({ ...r, component: Layout })) : []
+//       )
+//       flag.forEach((routerItem) => router.addRoute(routerItem))
+//     }
+//     next(to.path)
+//   } else {
+//     next()
+//   }
+// })
+
+const init = async () => {
+  const perms = ['shopping', 'shopping:goods', 'shopping:goods:list']
+  const permission = usePermission()
+  const accessRoutes: Array<RouteRecordRaw> = await permission.generateRoutes(perms)
+
+  const flag: Array<RouteRecordRaw> = accessRoutes.flatMap((item) =>
+    item.children ? item.children.map((r: any) => ({ ...r, component: Layout })) : []
+  )
+  flag.forEach((routerItem) => router.addRoute(routerItem))
+}
+
+export default init
