21a3b179deafa6391fc41ab1f7749e2380f74cd7
ferdinand <983901430@qq.com>
2024-05-09 15:38:14 +0800

    fix

--- src/layout/components/AppMain/Breadcrumb.vue
+++ src/layout/components/AppMain/Breadcrumb.vue
@@ -1,12 +1,43 @@
 <template>
-  <el-breadcrumb separator="/">
-    <el-breadcrumb-item :to="{ path: '/' }">homepage</el-breadcrumb-item>
-    <el-breadcrumb-item>
-      <a href="/">promotion management</a>
-    </el-breadcrumb-item>
-    <el-breadcrumb-item>promotion list</el-breadcrumb-item>
-    <el-breadcrumb-item>promotion detail</el-breadcrumb-item>
+  <el-breadcrumb separator=">">
+    <template v-for="(item, index) in breadList">
+      <el-breadcrumb-item v-if="item.name" :key="item.name" :test="index" :to="changeRoute(item)">
+        {{ item.meta?.name }}
+      </el-breadcrumb-item>
+    </template>
   </el-breadcrumb>
 </template>
 
-<script setup lang="ts" name="Breadcrumb"></script>
+<script setup lang="ts" name="Breadcrumb">
+import { ref, watch, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router'
+
+let route = useRoute()
+let breadList = ref<RouteRecordRaw[]>([])
+
+onMounted(() => {
+  getMatched()
+})
+
+watch(
+  () => route.path,
+  () => {
+    breadList.value = route.matched.filter((item) => item.meta && item.meta.name)
+  }
+)
+
+let getMatched = () => {
+  breadList.value = route.matched.filter((item) => item.meta && item.meta.name)
+}
+
+const changeRoute = (routerItem: RouteRecordRaw) => {
+  const path =
+    routerItem?.children?.[0]?.children?.[0]?.path ||
+    routerItem?.children?.[0]?.path ||
+    routerItem?.path ||
+    '/'
+
+  return path
+}
+</script>
