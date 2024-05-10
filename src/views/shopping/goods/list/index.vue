e7dbeb82fb954a8dea0953960740ea8577963c96
ferdinand <983901430@qq.com>
2024-05-10 16:12:15 +0800

    fix

--- src/views/shopping/goods/list/index.vue
+++ src/views/shopping/goods/list/index.vue
@@ -1,29 +1,13 @@
 <template>
-  <div>商品列表</div>
-  <button @click="test">按钮</button>
-  <div style="height: 500px">商品列表</div>
-  <div style="height: 500px">商品列表</div>
-  <div style="height: 500px">商品列表</div>
-  <div style="height: 500px">商品列表</div>
-  <div style="height: 500px">商品列表</div>
-  <div style="height: 500px">商品列表</div>
-  <div style="height: 500px">商品列表</div>
-  <div style="height: 500px">商品列表</div>
-  <div style="height: 500px">商品列表</div>
-  <div style="height: 500px">商品列表</div>
-  <div style="height: 500px">商品列表</div>
-  <div style="height: 500px">商品列表</div>
-  <div style="height: 500px">商品列表</div>
-  <div style="height: 500px">商品列表</div>
-  <div style="height: 500px">商品列表</div>
-  <div style="height: 500px">商品列表</div>
-  <div style="height: 500px">商品列表</div>
-  <div style="height: 500px">商品列表</div>
-  <div style="height: 500px">商品列表</div>
-  <div style="height: 500px">商品列表</div>
-  <div style="height: 500px">商品列表</div>
-  <div style="height: 500px">商品列表</div>
-  <div style="height: 500px">商品列表</div>
+  <el-main style="background-color: #ffffff">
+    <div>商品列表</div>
+    <button @click="test">按钮</button>
+    <el-table :data="tableData" stripe style="width: 100%">
+      <el-table-column prop="date" label="Date" />
+      <el-table-column prop="name" label="Name" />
+      <el-table-column prop="address" label="Address" />
+    </el-table>
+  </el-main>
 </template>
 
 <script setup lang="ts" name="GoodsList">
@@ -34,4 +18,11 @@ let Router = useRouter()
 const test = () => {
   Router.push('/shopping/goods/process')
 }
+
+const tableData = [
+  { date: '2016-05-03', name: 'Tom', address: 'No. 189, Grove St, Los Angeles' },
+  { date: '2016-05-02', name: 'Tom', address: 'No. 189, Grove St, Los Angeles' },
+  { date: '2016-05-04', name: 'Tom', address: 'No. 189, Grove St, Los Angeles' },
+  { date: '2016-05-01', name: 'Tom', address: 'No. 189, Grove St, Los Angeles' }
+]
 </script>
